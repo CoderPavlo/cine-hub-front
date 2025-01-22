ba6853b322d998fea0cd0983a1b61982f8276c28
CoderPavlo <pavlo4060@gmail.com>
2025-01-22 14:18:48 +0200

    clean project, set up routing and structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import MainPage from './pages/MainPage';
+
+export const role: 'admin' | 'user' = 'user'; //for demo
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div style={{ minHeight: '100hv', display: 'flex', flexDirection: 'column' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} /> {/*Main page*/}
+          <Route path="/login" element={<div />} />  {/*Add components for login instead div*/}
+          <Route path="/register" element={<div />} /> {/*Add components for register instead div*/}
+          {role === 'user' && <>
+
+            <Route path="/home" element={<div />} /> {/*Home page for user*/}
+            <Route path="/schedule" element={<div />} /> {/*Session schedule page for user*/}
+            <Route path="/movie/:id" element={<div />} /> {/*Movie details page*/}
+            <Route path="/session/:id" element={<div />} /> {/*Session detsils page for user*/}
+            <Route path="/booking/:id" element={<div />} /> {/*Booking page for user*/}
+            <Route path="/profile" element={<div />} /> {/*Profile page*/}
+          </>
+          }
+          {role === 'admin' && <>
+            <Route path="/admin-panel" element={<div />} /> {/*Admin panel page for admin*/}
+            <Route path="/statistics" element={<div />} /> {/*Statistics page for admin*/}
+            <Route path="/settings" element={<div />} /> {/*Settings page for admin*/}
+          </>}
+          <Route path="*" element={<Navigate to='/'/>} /> {/*Redirect to main page*/}
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App
